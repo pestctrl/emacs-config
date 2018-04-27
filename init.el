5d1fbb3dcae55b5339858ce7623a9bd95697f6b6
Benson Chu <bensonchu457@gmail.com>
2018-04-26 19:19:45 -0500

    Made use-package work, removed some use-packages for replacement with require

--- init.el
+++ init.el
@@ -1,15 +1,17 @@
 (setq package-list '(org use-package))
 
 ; list the repositories containing them
-(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "http://tromey.com/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 ; activate all the packages (in particular autoloads)
 (package-initialize)
 
+(when (not package-archive-contents)
+      (package-refresh-contents))
 ; fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -21,6 +23,9 @@
 
 (require 'org)
 (require 'use-package)
+
+(setq use-package-always-ensure t)
+
 (org-babel-load-file
  (expand-file-name "config.org"
                    user-emacs-directory))

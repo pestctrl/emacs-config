b1003b59756e810e39dcb4e81ea3f113e35aa716
Benson Chu <bensonchu457@gmail.com>
2019-03-24 12:10:17 -0500

    misc changes

--- agenda-files.el
+++ agenda-files.el
@@ -1,2 +1,2 @@
 
-(quote ("~/MEGA/org/agenda/reference.org" "~/MEGA/org/agenda/agenda.org" "~/MEGA/org/agenda/refile.org"))
+(quote ("~/MEGA/org/agenda/ContView.org" "~/MEGA/org/agenda/UHNW.org" "~/MEGA/org/agenda/read.org" "~/MEGA/org/agenda/agenda.org" "~/MEGA/org/agenda/reference.org" "~/MEGA/org/agenda/refile.org"))

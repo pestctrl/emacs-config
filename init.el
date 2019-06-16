f63dfc770cc0b97744329880ca058db5f4a19684
Benson Chu <bensonchu457@gmail.com>
2019-06-15 23:18:05 -0500

    Comments should be double semicolon

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (setq package-list '(org-plus-contrib use-package))
-; list the repositories containing them
+;; list the repositories containing them
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
@@ -7,16 +7,16 @@
                          ;;("marmalade" . "http://marmalade-repo.org/packages/")
                          ))
 
-; activate all the packages (in particular autoloads)
+;; activate all the packages (in particular autoloads)
 (package-initialize)
 
 (when (not package-archive-contents)
       (package-refresh-contents))
-; fetch the list of packages available 
+;; fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
 
-; install the missing packages
+;; install the missing packages
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))

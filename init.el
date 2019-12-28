063d19a86f00f2d3b7c0f67aa36582f0066cd77c
Benson Chu <bensonchu457@gmail.com>
2019-12-28 16:31:46 -0600

    Moved non-exwm keybindings to init.el

--- init.el
+++ init.el
@@ -41,6 +41,15 @@
 (define-prefix-command '*root-map*)
 (global-set-key (kbd "C-t") '*root-map*)
 
+(define-key *root-map* (kbd "C-n") 'switch-window)
+(define-key *root-map* (kbd "i") 'org-mru-clock-in)
+(define-key *root-map* (kbd "C-i") 'leaving-computer)
+(define-key *root-map* (kbd "C") 'org-resolve-clocks)
+(define-key *root-map* (kbd "j") 'org-clock-goto)
+(define-key *root-map* (kbd "o") 'switch-window)
+(define-key *root-map* (kbd "n") 'toggle-notifications)
+(define-key *root-map* (kbd "RET") 'dired-jump)
+
 ;; (org-babel-load-file
 ;;  (expand-file-name "config-exwm.org"
 ;;                    user-emacs-directory))

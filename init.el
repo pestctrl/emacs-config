77cce2e6189d5c6f31c99361faf2f60517ead78a
Benson Chu <bensonchu457@gmail.com>
2019-12-28 16:58:52 -0600

    Moved more keymaps

--- init.el
+++ init.el
@@ -41,6 +41,13 @@
 (define-prefix-command '*root-map*)
 (global-set-key (kbd "C-t") '*root-map*)
 
+(defconst my/keymap-key (kbd "C-t"))
+;; Disable C-t for all others
+(define-key vterm-mode-map (kbd "C-t") nil)
+(with-eval-after-load "ibuf-ext"
+  (define-key ibuffer-mode-map my/keymap-key nil))
+(define-key dired-mode-map my/keymap-key nil)
+
 (define-key *root-map* (kbd "C-n") 'switch-window)
 (define-key *root-map* (kbd "i") 'org-mru-clock-in)
 (define-key *root-map* (kbd "C-i") 'leaving-computer)

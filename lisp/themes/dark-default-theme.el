87f90a26791efc5247e517d9f92cc4443b73f91b
Benson Chu <bensonchu457@gmail.com>
2020-01-04 12:41:01 -0600

    Made my first theme!

---
+++ lisp/themes/dark-default-theme.el
@@ -0,0 +1,40 @@
+;;; dark-default.el ---  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Benson Chu
+
+;; Author: Benson Chu <bensonchu457@gmail.com>
+;; Created: [2020-01-02 15:06]
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(deftheme dark-default)
+
+(custom-theme-set-faces
+ 'dark-default
+ 
+ '(default ((t (:background "gray8" :foreground "#70FF00")))))
+
+;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'dark-default)
+;;; dark-default-theme.el ends here

d983736092c24436a225cd68699a0859449e2821
Benson Chu <bensonchu457@gmail.com>
2020-01-03 20:56:04 -0600

    I am the stupidest idiot. Of course there would be a built in impl

--- lisp/keymap.el
+++ lisp/keymap.el
@@ -26,5 +26,5 @@
 (define-key *root-map* (kbd "o") 'switch-window)
 (define-key *root-map* (kbd "n") 'toggle-notifications)
 (define-key *root-map* (kbd "RET") 'dired-jump)
-(define-key *root-map* (kbd "C-b") (lambda () (interactive) (switch-to-buffer (other-buffer (current-buffer) 1))))
+(define-key *root-map* (kbd "C-b") 'previous-buffer)
 

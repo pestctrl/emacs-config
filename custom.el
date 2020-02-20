7296d3a3a5f1cf7b7b954d1c478bfc62bdde12ff
Benson Chu <bensonchu457@gmail.com>
2020-02-19 19:43:11 -0600

    Changes to custom

--- custom.el
+++ custom.el
@@ -3,14 +3,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#c0c0c0" "#336c6c" "#806080" "#0f2050" "#732f2c" "#23733c" "#6c1f1c" "#232333"])
+ '(ansi-term-color-vector
+   [unspecified "#282936" "#ea51b2" "#ebff87" "#00f769" "#62d6e8" "#b45bcf" "#62d6e8" "#e9e9f4"] t)
+ '(beacon-color "#cc6666")
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(company-quickhelp-color-background "#b0b0b0")
+ '(company-quickhelp-color-foreground "#232333")
  '(custom-safe-themes
-   '("39d0bdce987fda3371df9cd3bb71a46228603a29ae21415e027b959a498bc46b" "331d55f96ad74cd9083f0c8f88ace2cd938d7e8deb9fb7f8a4f9ac6dc930517c" "5a65586d979afb677d509b31961c9eae49a1a6f77e5fdb8598ca37b50d72f617" "b1671363e11be28b1c5fc5ed912454bb5c75e41128005e3d21e89cd269789a1e" "47cab0d80005970023b859469ed1b50d4c8e30549988cb489d64430a280f4195" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8e797edd9fa9afec181efbfeeebf96aeafbd11b69c4c85fa229bb5b9f7f7e66c" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "bb5a253b1e359db941284cc74750ecf38aa93878b208919fc0f2199daaef491e" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "1a094b79734450a146b0c43afb6c669045d7a8a5c28bc0210aba28d36f85d86f" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" default))
+   '("73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "b8929cff63ffc759e436b0f0575d15a8ad7658932f4b2c99415f3dde09b32e97" "100eeb65d336e3d8f419c0f09170f9fd30f688849c5e60a801a1e6addd8216cb" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "672bb062b9c92e62d7c370897b131729c3f7fd8e8de71fc00d70c5081c80048c" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "39d0bdce987fda3371df9cd3bb71a46228603a29ae21415e027b959a498bc46b" "331d55f96ad74cd9083f0c8f88ace2cd938d7e8deb9fb7f8a4f9ac6dc930517c" "5a65586d979afb677d509b31961c9eae49a1a6f77e5fdb8598ca37b50d72f617" "b1671363e11be28b1c5fc5ed912454bb5c75e41128005e3d21e89cd269789a1e" "47cab0d80005970023b859469ed1b50d4c8e30549988cb489d64430a280f4195" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8e797edd9fa9afec181efbfeeebf96aeafbd11b69c4c85fa229bb5b9f7f7e66c" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "bb5a253b1e359db941284cc74750ecf38aa93878b208919fc0f2199daaef491e" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "1a094b79734450a146b0c43afb6c669045d7a8a5c28bc0210aba28d36f85d86f" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" default))
  '(debug-on-error nil)
  '(debug-on-quit nil)
  '(default-input-method "chinese-etzy")
  '(display-battery-mode t)
  '(display-time-mode t)
+ '(fci-rule-color "#c7c7c7")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
  '(hl-todo-keyword-faces
    '(("TODO" . "#dc752f")
      ("NEXT" . "#dc752f")
@@ -27,10 +39,13 @@
      ("FIXME" . "#dc752f")
      ("XXX+" . "#dc752f")
      ("\\?\\?\\?+" . "#dc752f")))
+ '(nrepl-message-colors
+   '("#336c6c" "#205070" "#0f2050" "#806080" "#401440" "#6c1f1c" "#6b400c" "#23733c"))
  '(org-agenda-files
    '("~/MEGA/org/2019-05-agenda/old_papers.org" "~/MEGA/org/2019-05-agenda/dev.org" "~/MEGA/org/2019-05-agenda/nci.org" "~/MEGA/org/2019-05-agenda/datetree.org" "~/MEGA/org/2019-05-agenda/what.org" "~/MEGA/org/2019-05-agenda/lists.org" "~/MEGA/org/entries/journal.gpg" "~/MEGA/org/2019-05-agenda/refile.org" "~/MEGA/org/2019-05-agenda/sandbox.org" "~/MEGA/org/2019-05-agenda/reference.org" "~/MEGA/org/2019-05-agenda/prod.org"))
  '(package-selected-packages
-   '(org-pomodoro company-tern banner-comments banner-comment try rustic flymake-rust flycheck-golangci-lint flycheck-inline flycheck-rust helm-org memory-usage god-mode ivy-rich doom elixir-mode gruvbox slime-company w3m edwina treefactor elfeed-web workgroups2 unicode-fonts emms mmt sclang-snippets sclang-extensions 2048-game org-loop google-translate persp-mode switch-buffer-functions org-sidebar zeal-at-point org-drill org-re-reveal company-tabnine hdfs-tramp vterm tramp-hdfs nyan-mode parrot doom-modeline centaur-tabs solaire-mode company-distel company-erlang erlang magit-todos twittering-mode spacemacs-theme oauth2 offlineimap cyberpunk-2019-theme ebdb weechat notmuch frog-jump-buffer quelpa-use-package quelpa orca poshint f3 helm-fuzzy-find e2wm exwm exwm-firefox-core ivy-posframe dockerfile-mode docker realgud gruvbox-theme eterm-256color iy-go-to-char ivy ivy-hydra exwm-edit w3 dired-collapse dired-subtree dired-filter google-this strace-mode ten-hundred-mode jupyter hackernews academic-phrases racket-mode flymd telephone-line cnfonts names ctable c-c-combo doom-themes helpful helm-org-rifle all-the-icons speed-type proof-general webpaste erc-hl-nick java-snippets mvn pass erc-hl-nicks erc-colorize bbdb- bbdb-ext erc gnorb bbdb window-purpose tide company-flow tern-auto-complete tern dotnet dotnet-mode elisp-def auto-highlight-symbol ccls xcscope ggtags indium cargo gradle-mode kdeconnect hy-mode hacker-typer csv-mode org-bookmark-heading system-packages org-projectile treemacs-projectile expand-region org-wunderlist lua-mode treemacs-icons-dired memoize skeletor 0xc lsp-rust smart-mode-line-powerline-theme eclim alert gscholar-bibtex org-noter pdf-tools md4rd arch-packer interleave sx desktop-environment org-journal-list indent-guide org-board org-brain org-edna org-clock-csv org-clock-convenience org-mru-clock lsp-ui dap-mode lsp-python lsp-java lsp-ruby lsp-mode smart-jump scrollkeeper elfeed-org elfeed rg ag dumb-jump rainbow-delimiters org-super-agenda htmlize exwm-x keyfreq org-jira eyebrowse omnisharp omnisharp-emacs typescript-mode csharp-mode restclient rjsx-mode go-mode go treemacs org-timeline plantuml-mode pkgbuild-mode airline-themes powerline magit-svn ein emacs-ipython-notebook processing-mode web-mode ledger-mode elpy skewer-mode js2-mode dired-narrow yaml-mode clj-refactor paredit geiser magit ensime yasnippet irony company dired-du org-caldav calfw-org calfw-gcal calfw-ical calfw org-bullets ace-jump-mode switch-window counsel smex helm counsel-projectile helm-projectile projectile slime pulseaudio-control evil undo-tree multi-term swiper color-theme-modern org use-package))
+   '(olivetti anti-zenburn-theme base16-theme color-theme-sanityinc-tomorrow leuven-theme pretty-symbols org-wiki org-pomodoro company-tern banner-comments banner-comment try rustic flymake-rust flycheck-golangci-lint flycheck-inline flycheck-rust helm-org memory-usage god-mode ivy-rich doom elixir-mode gruvbox slime-company w3m edwina treefactor elfeed-web workgroups2 unicode-fonts emms mmt sclang-snippets sclang-extensions 2048-game org-loop google-translate persp-mode switch-buffer-functions org-sidebar zeal-at-point org-drill org-re-reveal company-tabnine hdfs-tramp vterm tramp-hdfs nyan-mode parrot doom-modeline centaur-tabs solaire-mode company-distel company-erlang erlang magit-todos twittering-mode spacemacs-theme oauth2 offlineimap cyberpunk-2019-theme ebdb weechat notmuch frog-jump-buffer quelpa-use-package quelpa orca poshint f3 helm-fuzzy-find e2wm exwm exwm-firefox-core ivy-posframe dockerfile-mode docker realgud gruvbox-theme eterm-256color iy-go-to-char ivy ivy-hydra exwm-edit w3 dired-collapse dired-subtree dired-filter google-this strace-mode ten-hundred-mode jupyter hackernews academic-phrases racket-mode flymd telephone-line cnfonts names ctable c-c-combo doom-themes helpful helm-org-rifle all-the-icons speed-type proof-general webpaste erc-hl-nick java-snippets mvn pass erc-hl-nicks erc-colorize bbdb- bbdb-ext erc gnorb bbdb window-purpose tide company-flow tern-auto-complete tern dotnet dotnet-mode elisp-def auto-highlight-symbol ccls xcscope ggtags indium cargo gradle-mode kdeconnect hy-mode hacker-typer csv-mode org-bookmark-heading system-packages org-projectile treemacs-projectile expand-region org-wunderlist lua-mode treemacs-icons-dired memoize skeletor 0xc lsp-rust smart-mode-line-powerline-theme eclim alert gscholar-bibtex org-noter pdf-tools md4rd arch-packer interleave sx desktop-environment org-journal-list indent-guide org-board org-brain org-edna org-clock-csv org-clock-convenience org-mru-clock lsp-ui dap-mode lsp-python lsp-java lsp-ruby lsp-mode smart-jump scrollkeeper elfeed-org elfeed rg ag dumb-jump rainbow-delimiters org-super-agenda htmlize exwm-x keyfreq org-jira eyebrowse omnisharp omnisharp-emacs typescript-mode csharp-mode restclient rjsx-mode go-mode go treemacs org-timeline plantuml-mode pkgbuild-mode airline-themes powerline magit-svn ein emacs-ipython-notebook processing-mode web-mode ledger-mode elpy skewer-mode js2-mode dired-narrow yaml-mode clj-refactor paredit geiser magit ensime yasnippet irony company dired-du org-caldav calfw-org calfw-gcal calfw-ical calfw org-bullets ace-jump-mode switch-window counsel smex helm counsel-projectile helm-projectile projectile slime pulseaudio-control evil undo-tree multi-term swiper color-theme-modern org use-package))
+ '(pdf-view-midnight-colors '("#232333" . "#c7c7c7"))
  '(projectile-enable-caching t)
  '(safe-local-variable-values
    '((projectile-project-run-cmd . "dotnet run --project ContentViewer/ContentViewer.csproj")
@@ -43,6 +58,28 @@
      (projectile-project-run-cmd . "docker run doclockselen")))
  '(send-mail-function 'smtpmail-send-it)
  '(tooltip-mode nil)
+ '(vc-annotate-background "#d4d4d4")
+ '(vc-annotate-color-map
+   '((20 . "#437c7c")
+     (40 . "#336c6c")
+     (60 . "#205070")
+     (80 . "#2f4070")
+     (100 . "#1f3060")
+     (120 . "#0f2050")
+     (140 . "#a080a0")
+     (160 . "#806080")
+     (180 . "#704d70")
+     (200 . "#603a60")
+     (220 . "#502750")
+     (240 . "#401440")
+     (260 . "#6c1f1c")
+     (280 . "#935f5c")
+     (300 . "#834744")
+     (320 . "#732f2c")
+     (340 . "#6b400c")
+     (360 . "#23733c")))
+ '(vc-annotate-very-old-color "#23733c")
+ '(window-divider-mode nil)
  '(wtf-custom-alist
    '(("AES" . "Advanced Encryption Standard")
      ("PDF" . "Portable Document Format")
@@ -54,4 +91,5 @@
  ;; If there is more than one, they won't work right.
  '(notmuch-tree-match-tree-face ((t (:family "DejaVu Sans Mono"))) t)
  '(notmuch-tree-no-match-tree-face ((t (:family "DejaVu Sans Mono"))) t)
- '(sml/charging ((t (:inherit sml/global :foreground "forest green")))))
+ '(sml/charging ((t (:inherit sml/global :foreground "forest green"))))
+ '(variable-pitch ((t (:slant normal :weight normal :height 120 :width normal :foundry "ADBO" :family "Source Sans Pro")))))

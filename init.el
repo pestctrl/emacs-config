1b6574e1daf4c7edd7b6a720d3cfbd5f4f2ed949
Benson Chu <bensonchu457@gmail.com>
2021-12-19 16:43:06 -0600

    Org elpa being deprecated

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (setq package-list '(org use-package))
 ;; list the repositories containing them
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
+;;                         ("org" . "https://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("elpa" . "http://tromey.com/elpa/")
                          ;;("marmalade" . "http://marmalade-repo.org/packages/")

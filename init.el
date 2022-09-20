15ec22680ac4bd3274a0a6da12344bd9c6a19101
Puppet <NightAniMer@hotmail.com>
2022-09-20 21:12:49 +0300

    WSL now opens stuff in windows
    However, what else is setting browse-url-browser-function?

--- init.el
+++ init.el
@@ -117,6 +117,15 @@
   (when my-ec/enable-exwm
     (require 'exwm)))
 
+(when my-ec/is-wsl
+  (let ((cmd-exe "/mnt/c/Windows/System32/cmd.exe")
+        (cmd-args '("/c" "start")))
+    (when (file-exists-p cmd-exe)
+      (setq browse-url-generic-program  cmd-exe
+            browse-url-generic-args     cmd-args
+            browse-url-browser-function 'browse-url-generic
+            search-web-default-browser 'browse-url-generic))))
+
 (setq my/finished t)
 ;; Testing pull from windows
 

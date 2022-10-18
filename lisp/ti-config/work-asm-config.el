6bb928c9cfff247b7104e71f9e4ddc14038be846
Benson Chu <b-chu1@ti.com>
2022-10-18 08:50:05 -0500

    Try to optimize for C29 asm

--- lisp/ti-config/work-asm-config.el
+++ lisp/ti-config/work-asm-config.el
@@ -30,45 +30,62 @@
           (lambda ()
             (setq tab-width 10)))
 
+;; (setq asm-font-lock-keywords
+;;       (append
+;;        (list '("^\\(\\(\\sw\\|\\s_\\)+\\)\\>:?[ \t]*\\(\\sw+\\(\\.\\sw+\\)*\\)?"
+;;                (1 font-lock-function-name-face) (3 font-lock-keyword-face nil t))
+;;              label started from ".".
+;;              `(,(rx line-start (optional (= 8 (any "0-9A-f"))) (group "." (+ (or (syntax word) (syntax symbol)))) symbol-end ":")
+;;                (1 font-lock-function-name-face))
+;;              `(,(rx line-start (* space) "||" (+ space)) . 'asm-vliw-bar)
+;;              `(,(rx line-start
+;;                     (optional (* space) (+ alphanumeric)) (+ space)
+;;                     (optional "||" (+ space))
+;;                     (group
+;;                      (group
+;;                       (+ (or (syntax symbol)
+;;                              (and (optional ".")
+;;                                   (syntax word)))))
+;;                      (group
+;;                       (* (and "." (+ (syntax word))))))
+;;                     (+ space)
+;;                     nonl)
+;;                (1 font-lock-keyword-face))
+;;              `(,(rx line-start
+;;                     (optional (group "(" (+ (syntax word)) ")"))
+;;                     (+ (syntax whitespace))
+;;                     (group (group (+ (or (and (optional ".") (syntax word))
+;;                                          (syntax symbol))))
+;;                            (* (group "." (+ (syntax word))))))
+;;                (2 font-lock-keyword-face))
+;;              directive started from ".".
+;;              '("^\\(\\.\\(\\sw\\|\\s_\\)+\\)\\>[^:]?"
+;;                1 font-lock-keyword-face)
+;;              %register
+;;              `(,(rx "ADDR1") . font-lock-type-face)
+;;              `(,(rx (or "TA" "TDM" "M" "A" "D")
+;;                     (or (+ digit) ".GT" ".LT" ".EQ"))
+;;                . font-lock-variable-name-face)
+;;              `(,(rx "@" (optional "($") (group (+ (or (syntax word) (syntax symbol)))) (or ")" symbol-end)) (1 font-lock-function-name-face))
+;;              `(,(rx "#" (optional "-") "0x" (+ alphanumeric)) . font-lock-constant-face))
+;;        cpp-font-lock-keywords))
+
 (setq asm-font-lock-keywords
-      (append
-       (list '("^\\(\\(\\sw\\|\\s_\\)+\\)\\>:?[ \t]*\\(\\sw+\\(\\.\\sw+\\)*\\)?"
-               (1 font-lock-function-name-face) (3 font-lock-keyword-face nil t))
-             ;; label started from ".".
-             `(,(rx line-start (group "." (+ (or (syntax word) (syntax symbol)))) symbol-end ":")
-               (1 font-lock-function-name-face))
-             `(,(rx line-start (* space) "||" (+ space)) . 'asm-vliw-bar)
-             `(,(rx line-start
-                    (optional (* space) (+ alphanumeric)) (+ space)
-                    (optional "||" (+ space))
-                    (group
-                     (group
-                      (+ (or (syntax symbol)
-                             (and (optional ".")
-                                  (syntax word)))))
-                     (group
-                      (* (and "." (+ (syntax word))))))
-                    (+ space)
-                    nonl)
-               (1 font-lock-keyword-face))
-             `(,(rx line-start
-                    (optional (group "(" (+ (syntax word)) ")"))
-                    (+ (syntax whitespace))
-                    (group (group (+ (or (and (optional ".") (syntax word))
-                                         (syntax symbol))))
-                           (* (group "." (+ (syntax word))))))
-               (2 font-lock-keyword-face))
-             ;; directive started from ".".
-             '("^\\(\\.\\(\\sw\\|\\s_\\)+\\)\\>[^:]?"
-               1 font-lock-keyword-face)
-             ;; %register
-             `(,(rx "ADDR1") . font-lock-type-face)
-             `(,(rx (or "TA" "TDM" "M" "A" "D")
-                    (or (+ digit) ".GT" ".LT" ".EQ"))
-               . font-lock-variable-name-face)
-             `(,(rx "@" (optional "($") (group (+ (or (syntax word) (syntax symbol)))) (or ")" symbol-end)) (1 font-lock-function-name-face))
-             `(,(rx "#" (optional "-") "0x" (+ alphanumeric)) . font-lock-constant-face))
-       cpp-font-lock-keywords))
+      `((,(rx line-start (group (= 8 (any "0-9A-f"))) " " (group (+ (or (syntax word) (syntax symbol)))) symbol-end ":")
+         (1 font-lock-warning-face) (2 font-lock-function-name-face))
+        (,(rx line-start (group (= 8 (any "0-9A-f"))) ":" (+ space) (group (+ (and  (= 4 (any "0-9A-f")) (+ space)))))
+         (1 font-lock-warning-face) (2 font-lock-constant-face))
+        (,(rx line-start (* (+ (any "0-9A-f")) (+ (or ":" space))) (+ space) (optional "||" (+ space)) (group (+ (or (syntax word) (syntax symbol) "."))))
+         (1 font-lock-keyword-face))
+        (,(rx line-start (group (+  (or (syntax word) (syntax symbol)))) ":") (1 font-lock-function-name-face))
+        (,(rx "ADDR1") . font-lock-type-face)
+        (,(rx (or "TA" "TDM" "M" "A" "D")
+               (or (+ digit) ".GT" ".LT" ".EQ"))
+          . font-lock-variable-name-face)
+        (,(rx "@" (optional "(") (group (optional "(") (+ (or (syntax word) (syntax symbol)))) (or ")" symbol-end)) (1 font-lock-function-name-face))
+        (,(rx "#" (optional "-") "0x" (+ alphanumeric)) . font-lock-constant-face)
+        ;; (,(rx (* space) "\n" (* space) "||" (+ space)) . 'asm-vliw-bar)
+        ))
 
 (defface asm-vliw-bar `((t (:background "gray25" :extend t :inherit font-lock-comment-face))) nil)
 

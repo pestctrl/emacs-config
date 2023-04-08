c605f2fa487937546bbcd9cb2c7a72c02c4af78c
Benson Chu <bensonchu457@gmail.com>
2023-04-08 12:28:12 -0500

    Vertico jumper

---
+++ lisp/vertico-jumper.el
@@ -0,0 +1,40 @@
+;;; vertico-jumper.el ---  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Benson Chu
+
+;; Author: Benson Chu <bensonchu457@gmail.com>
+;; Created: [2023-03-31 09:21]
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(use-package vertico)
+(require 'vertico-flat)
+
+(define-prefix-command '*vertico-jump-map*)
+
+(define-key vertico-flat-map (kbd "M-j") '*vertico-jump-map*)
+
+(define-key *vertico-jump-map* (kbd "h") #'vertico--jump-to-home)
+
+(defun vertico--jump-to-home ()
+  (interactive)
+  (insert "~/"))
+
+(provide 'vertico-jumper)
+;;; vertico-jumper.el ends here

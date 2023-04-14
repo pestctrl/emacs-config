a53842116d8a807d6520c133047ef1e941606b31
Benson Chu <bensonchu457@gmail.com>
2023-04-13 21:55:31 -0500

    Now using rx-to-string, can added empty fill

--- lisp/ti-config/work-commentor.el
+++ lisp/ti-config/work-commentor.el
@@ -38,13 +38,13 @@
 
 (defclass banner-comment-style ()
   ((align :initarg :align :type symbol :initform 'left)
-   (fill :initarg :fill :type character :initform ? )
+   (fill :initarg :fill :type character :initform (string-to-char " "))
    (empty-fill :initarg :empty-fill)
    (padding :initarg :padding :type string :initform "")
    (start :initarg :start :initform nil)
    (end :initarg :end :initform nil)))
 
-(cl-defun banner/new-style (sym &key (align 'left) (fill ? ) (padding "") start end empty)
+(cl-defun banner/new-style (sym &key (align 'left) (fill (string-to-char " ")) (padding "") start end empty)
   (add-to-list 'banner/styles
                (cons sym
                      (make-instance
@@ -53,14 +53,16 @@
                       :start start :end end :empty-fill (or empty fill)))))
 
 (banner/new-style 'llvm-start :padding "===" :fill ?- :end 'comment-start)
-(banner/new-style 'ti-box :fill ? :empty ?- :end 'comment-start)
+(banner/new-style 'ti-box
+                  :fill (string-to-char " ")
+                  :empty ?- :end 'comment-start)
 
 (defun banner/change-alignment (align)
   (interactive (list (intern (completing-read "Alignment? " '(left right center)))))
   (setf (slot-value banner/current-style 'align)
         align))
 
-;; (setq banner/current-style (cdr (car banner/styles)))
+;; (setq banner/current-style (cdr (car banner/styles)))            ;;
 
 (defun banner/extract-comment-string (expr)
   (cond ((null expr) (banner/extract-comment-string comment-start))
@@ -76,41 +78,49 @@
     (or banner/current-style
         (cdr (car banner/styles)))))
   (with-slots (align fill empty-fill padding start end) style
-    (let* ((empty-fill (or (and arg fill)
-                           empty-fill))
-           (comm-start (banner/extract-comment-string start))
+    (let* ((comm-start (banner/extract-comment-string start))
            (comm-end (banner/extract-comment-string end))
            (regexp
-            (rx line-start
-                (group
-                 (literal comm-start)
-                 (literal padding)
-                 (+ (literal (string fill)))
-                 (* (literal (if (eq fill ? ) "" " "))))
-                (group
-                 (*? nonl))
-                (group
-                 (* (literal (if (eq fill ? ) "" " ")))
-                 (+ (literal (string fill)))
-                 (literal (string-reverse padding))
-                 (literal comm-end))
-                line-end)))
+            (rx-to-string
+             `(and
+               line-start
+               (group (* " "))
+               (group
+                (optional
+                 ,comm-start
+                 ,padding
+                 (or (+ ,(string empty-fill)) (+ ,(string fill)))
+                 (* " ")))
+               (group
+                (*? (not ,fill)))
+               (group
+                (optional
+                 (* " ")
+                 (or (+ ,(string empty-fill)) (+ ,(string fill)))
+                 ,(string-reverse padding)
+                 ,comm-end))
+               line-end))))
       (save-excursion
         (goto-char (point-at-bol))
         (when (or (looking-at regexp)
-                  (looking-at (rx line-start
-                                  (group (* " "))
-                                  (group (+ nonl))
-                                  (group (* " "))
-                                  line-end))
-                  (looking-at (rx line-start
-                                  (group (* " "))
-                                  (group) (group)
-                                  line-end)))
-          (let* ((len (length (match-string 2)))
+                  ;; (looking-at (rx line-start
+                  ;;                 (group (* " "))
+                  ;;                 (group)
+                  ;;                 (group (+ nonl))
+                  ;;                 (group (* " "))
+                  ;;                 line-end))
+                  ;; (looking-at (rx line-start
+                  ;;                 (group (* " "))
+                  ;;                 (group) (group) (group)
+                  ;;                 line-end))
+                  )
+          (let* ((empty-fill (or (and arg fill)
+                                 empty-fill))
+                 (len (length (match-string 3)))
                  (space-pad (and (not (= len 0))
-                                 (not (eq ?  fill))))
+                                 (not (eq (string-to-char " ") fill))))
                  (fill-left (- fill-column len
+                               (length (match-string 1))
                                (if space-pad 2 0)
                                (* 2 (length padding))
                                (length comm-start)
@@ -130,14 +140,16 @@
                                    (make-string left (or (and (= len 0) empty-fill)
                                                          fill))
                                    (when space-pad " "))
-                           nil nil nil 1)
+                           nil nil nil 2)
 
             (when (and padding comm-end)
               (replace-match (concat (when space-pad " ")
                                      (make-string right (or (and (= len 0) empty-fill)
                                                             fill))
                                      (string-reverse padding) comm-end)
-                             nil nil nil 3))))))))
+                             nil nil nil 4))))))))
+;;------------------------------------------------------------------;;
+;; Hello                                                            ;;
 
 (provide 'work-commentor)
 ;;; work-commentor.el ends here

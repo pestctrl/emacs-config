d5a4825082f7dd8f6991dd916641f9e89e9dace9
Benson Chu <b-chu1@ti.com>
2023-05-07 16:26:52 -0500

    Tab patch part 2

--- lisp/tab-bar-patch.el
+++ lisp/tab-bar-patch.el
@@ -28,6 +28,117 @@
             :override
             #'my/tab-bar-new-tab-to)
 
+(advice-add #'tab-bar-select-tab
+            :override
+            #'my/tab-bar-select-tab)
+
+(defun my/tab-bar-select-tab (&optional tab-number)
+  "Switch to the tab by its absolute position TAB-NUMBER in the tab bar.
+When this command is bound to a numeric key (with a key prefix or modifier key
+using `tab-bar-select-tab-modifiers'), calling it without an argument
+will translate its bound numeric key to the numeric argument.
+Also the prefix argument TAB-NUMBER can be used to override
+the numeric key, so it takes precedence over the bound digit key.
+For example, `<MODIFIER>-2' will select the second tab, but `C-u 15
+<MODIFIER>-2' will select the 15th tab.  TAB-NUMBER counts from 1.
+Negative TAB-NUMBER counts tabs from the end of the tab bar."
+  (interactive "P")
+  (unless (integerp tab-number)
+    (let ((key (event-basic-type last-command-event)))
+      (setq tab-number (if (and (characterp key) (>= key ?1) (<= key ?9))
+                           (- key ?0)
+                         0))))
+
+  (let* ((tabs (funcall tab-bar-tabs-function))
+         (from-index (tab-bar--current-tab-index tabs))
+         (to-number (cond ((< tab-number 0) (+ (length tabs) (1+ tab-number)))
+                          ((zerop tab-number) (1+ from-index))
+                          (t tab-number)))
+         (to-index (1- (max 1 (min to-number (length tabs)))))
+         (minibuffer-was-active (minibuffer-window-active-p (selected-window))))
+
+    (unless (eq from-index to-index)
+      (let* ((from-tab (tab-bar--tab))
+             (to-tab (nth to-index tabs))
+             (wc (alist-get 'wc to-tab))
+             (ws (alist-get 'ws to-tab)))
+
+        ;; During the same session, use window-configuration to switch
+        ;; tabs, because window-configurations are more reliable
+        ;; (they keep references to live buffers) than window-states.
+        ;; But after restoring tabs from a previously saved session,
+        ;; its value of window-configuration is unreadable,
+        ;; so restore its saved window-state.
+        (cond
+         ((and (window-configuration-p wc)
+               ;; Check for such cases as cloning a frame with tabs.
+               ;; When tabs were cloned to another frame, then fall back
+               ;; to using `window-state-put' below.
+               (eq (window-configuration-frame wc) (selected-frame)))
+          (let ((wc-point (alist-get 'wc-point to-tab))
+                (wc-bl  (seq-filter #'buffer-live-p (alist-get 'wc-bl to-tab)))
+                (wc-bbl (seq-filter #'buffer-live-p (alist-get 'wc-bbl to-tab)))
+                (wc-history-back (alist-get 'wc-history-back to-tab))
+                (wc-history-forward (alist-get 'wc-history-forward to-tab)))
+
+            (set-window-configuration wc nil t)
+
+            ;; set-window-configuration does not restore the value of
+            ;; point in the current buffer, so restore it separately.
+            (when (and (markerp wc-point)
+                       (marker-buffer wc-point)
+                       ;; FIXME: After dired-revert, marker relocates to 1.
+                       ;; window-configuration restores point to global point
+                       ;; in this dired buffer, not to its window point,
+                       ;; but this is slightly better than 1.
+                       ;; Maybe better to save dired-filename in each window?
+                       (not (eq 1 (marker-position wc-point))))
+              (goto-char wc-point))
+
+            (when wc-bl  (set-frame-parameter nil 'buffer-list wc-bl))
+            (when wc-bbl (set-frame-parameter nil 'buried-buffer-list wc-bbl))
+
+            (when tab-bar-history-mode
+              (puthash (selected-frame)
+                       (and (window-configuration-p (alist-get 'wc (car wc-history-back)))
+                            wc-history-back)
+                       tab-bar-history-back)
+              (puthash (selected-frame)
+                       (and (window-configuration-p (alist-get 'wc (car wc-history-forward)))
+                            wc-history-forward)
+                       tab-bar-history-forward))))
+
+         (ws
+          ;; `window-state-put' fails when called in the minibuffer
+          (when (minibuffer-selected-window)
+            (select-window (minibuffer-selected-window)))
+          (let ((window--sides-inhibit-check t))
+            (set-window-parameter (selected-window) 'window-side nil)
+            (window-state-put ws nil 'safe))))
+
+        ;; Select the minibuffer when it was active before switching tabs
+        (when (and minibuffer-was-active (active-minibuffer-window))
+          (select-window (active-minibuffer-window)))
+
+        ;; When the minibuffer was activated in one tab, but exited in
+        ;; another tab, then after going back to the first tab, it has
+        ;; such inconsistent state that the current buffer is the minibuffer,
+        ;; but its window is not active.  So try to undo this mess.
+        (when (and (minibufferp) (not (active-minibuffer-window)))
+          (other-window 1))
+
+        (when tab-bar-history-mode
+          (setq tab-bar-history-omit t))
+
+        (when from-index
+          (setf (nth from-index tabs) from-tab))
+        (setf (nth to-index tabs) (tab-bar--current-tab-make (nth to-index tabs)))
+
+        (unless tab-bar-mode
+          (message "Selected tab '%s'" (alist-get 'name to-tab))))
+
+      (force-mode-line-update))))
+
 (defun my/tab-bar-new-tab-to (&optional tab-number)
   (interactive "P")
   (let* ((tabs (funcall tab-bar-tabs-function))
@@ -45,6 +156,7 @@
             (window-state-put (window-state-get))
           ;; Remove window parameters that can cause problems
           ;; with `delete-other-windows' and `split-window'.
+          (set-window-parameter nil 'window-side nil)
           (set-window-parameter nil 'window-atom nil)
           (delete-other-windows)
           (if (eq tab-bar-new-tab-choice 'window)

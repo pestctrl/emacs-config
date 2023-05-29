49a572fa74086794ff55f80a3f07a12d2df8262e
Benson Chu <bensonchu457@gmail.com>
2023-05-29 10:56:17 -0500

    Removed old files

--- agenda-files.el
+++
@@ -1,2 +0,0 @@
-
-(quote ("~/MEGA/org/agenda/research.org" "~/MEGA/org/agenda/long_term.org" "~/MEGA/org/agenda/ContView.org" "~/MEGA/org/agenda/UHNW.org" "~/MEGA/org/agenda/read.org" "~/MEGA/org/agenda/agenda.org" "~/MEGA/org/agenda/reference.org" "~/MEGA/org/agenda/refile.org"))

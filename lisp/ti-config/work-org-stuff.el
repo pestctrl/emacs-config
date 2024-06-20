b121cbd40a0e73b0ef2a90a5e301857185b9df71
Benson Chu <b-chu1@ti.com>
2024-06-20 14:17:57 -0500

    Indentation

--- lisp/ti-config/work-org-stuff.el
+++ lisp/ti-config/work-org-stuff.el
@@ -604,12 +604,12 @@
 
   (defvar my/project-templates
     '(("p" "project" plain ""
-        :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                           "#+title: ${title}: %^{Description}\n#+category: ${title}\n#+filetags: Project active")
-        :unnarrowed t)
-       ("s" "sandbox" plain ""
-        :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                           "#+title: ${title}: %^{Description}\n#+category: ${title}\n#+filetags: Project active\n#+PROPERTY: header-args:bash :dir /scratch/benson/sandbox/${title} :results output verbatim :exports results :noweb yes"))))
+       :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                          "#+title: ${title}: %^{Description}\n#+category: ${title}\n#+filetags: Project active")
+       :unnarrowed t)
+      ("s" "sandbox" plain ""
+       :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                          "#+title: ${title}: %^{Description}\n#+category: ${title}\n#+filetags: Project active\n#+PROPERTY: header-args:bash :dir /scratch/benson/sandbox/${title} :results output verbatim :exports results :noweb yes"))))
 
   (defun my/org-roam-find-projects ()
     (interactive)

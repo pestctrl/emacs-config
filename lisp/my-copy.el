0cc2784135d69e0d02df1d326919242131ec6510
Benson Chu <b-chu1@ti.com>
2024-10-08 15:26:22 -0500

    Copy file name

---
+++ lisp/my-copy.el
@@ -0,0 +1,36 @@
+;;; my-copy.el ---  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Benson Chu
+
+;; Author: Benson Chu <bensonchu457@gmail.com>
+;; Created: [2024-10-08 09:32]
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(require 'my-keymap)
+
+(defun my/dynamic-yank ()
+  (interactive)
+  (when-let ((b (buffer-file-name)))
+    (kill-new b)))
+
+(define-key *root-map* (kbd "M-w") #'my/dynamic-yank)
+
+(provide 'my-copy)
+;;; my-copy.el ends here
